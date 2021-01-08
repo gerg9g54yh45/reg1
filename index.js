85896f1e48a4f73436e69cfd5af9e6abbc25e94b
Vlad <berserk-li@mail.ru>
2021-01-08 11:10:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ const leaderbordMess = {
 
 const Config = require('./configs.js')
 const config = Config.exports || Config
-const TIMELENGTH = 1000 * 60 * 60 * 11 // 7 дней - длительность хаба
-const TIMESTART = 1609851600000 // старт (16:00)
+const TIMELENGTH = 1000 * 60 * 60 * 9 // 7 часов - длительность хаба
+const TIMESTART = 1610107200000 // старт (15:00)
 // const TIMECHECK = 1000 * 60 * 60 * 2 // 2 часа - как часто проверять команду (историю)
 const TIMECHECK = 1000 * 60 * 15
 const TIMEDURATIONMATCH = 1000 * 60 * 60 * 3 // время длительности старта без погрешности (для самих матчей)
@@ -748,7 +748,7 @@ function hubLeaderbordUpdate() {
 
     // обновляем текст сообщений лидерборда
     // leaderbordMess.adv.body.edit(messageSoloLeaderbord)
-    // leaderbordMess.pro.body.edit(messageTrioLeaderbord)
+    leaderbordMess.pro.body.edit(messageTrioLeaderbord)
 }
 
 
@@ -835,5 +835,5 @@ function wrapperLimiter(func, time=1000) {
 function getNewDate() {
     return new Date( +new Date() + 1000 * 60 * 60 * 3 )
 }
-// setInterval(hubLeaderbordUpdate, 1000 * 60 * 5) // каждые 5 минут обновление лидерборда матчей
-// setInterval(startCheckAllMatches, 1000 * 60 * 30) // каждые 30 минут чекаем стату всех матчей
+setInterval(hubLeaderbordUpdate, 1000 * 60 * 5) // каждые 5 минут обновление лидерборда матчей
+setInterval(startCheckAllMatches, 1000 * 60 * 30) // каждые 30 минут чекаем стату всех матчей
